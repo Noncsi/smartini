dcf9c76c0473bd2e7b025909586114b877b07251
Noncsi <lengyel.noemi96@gmail.com>
2024-08-14 15:30:40 +0200

    transform basic example to fit Angular + make it accessible by other devices

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'smartini';
+export class AppComponent implements OnInit {
+  title: string = 'smartini';
+  socket: WebSocket = new WebSocket('ws://192.168.0.103:8080');
+  message: string = '';
+  receivedMessage: string = '';
+
+  ngOnInit(): void {
+    this.socket.onopen = (e) => {
+      alert('You are Connected to WebSocket Server');
+    };
+
+    this.socket.onmessage = (e) => {
+      this.receivedMessage = `Received "${e.data}" from server.`;
+    };
+
+    this.socket.onclose = (e) => {
+      console.log('Disconnected from WebSocket server');
+    };
+  }
+
+  sendMessage() {
+    this.socket.send(this.message);
+    this.message = '';
+  }
 }
