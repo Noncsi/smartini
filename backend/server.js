ca5fb2f8f17b513a0d054e93a026b7a1c5a220f0
Noncsi <lengyel.noemi96@gmail.com>
2024-08-15 23:25:06 +0200

    add typescript to node.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,35 @@
-const WebSocket = require("ws");
-const clientWs = new WebSocket.Server({ port: 8080 });
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = __importDefault(require("ws"));
+const clientWs = new ws_1.default.Server({ port: 8080 });
 let counter = 0;
 console.log("Server is running...");
-
 const clients = new Map();
-
 clientWs.on("connection", function connection(ws) {
-  const id = counter++;
-  console.log("Player connected", id);
-  const color = Math.floor(Math.random() * 360);
-  const metadata = { color, ws };
-
-  clients.set(id, metadata);
-
-  ws.on("message", function incoming(name) {
-    console.log("Player received: %s", name);
-    const message = JSON.parse(name);
-    const metadata = clients.get(ws);
-
-    // message.sender = metadata.id;
-    // message.color = metadata.color;
-
-    const outbound = JSON.stringify(message);
-
-    [...clients.keys()].forEach((key) => {
-      if (key === 0) {
-        const a = clients.get(0);
-        a.ws.send(outbound);
-      }
+    const id = counter++;
+    console.log("Player connected", id);
+    const color = Math.floor(Math.random() * 360);
+    const metadata = { color, ws };
+    clients.set(id, metadata);
+    ws.on("message", function incoming(name) {
+        console.log("Player received: %s", name);
+        const message = JSON.parse(name);
+        const metadata = clients.get(ws);
+        // message.sender = metadata.id;
+        // message.color = metadata.color;
+        const outbound = JSON.stringify(message);
+        [...clients.keys()].forEach((key) => {
+            if (key === 0) {
+                const a = clients.get(0);
+                a.ws.send(outbound);
+            }
+        });
     });
-  });
-
-  ws.on("close", function () {
-    console.log("Player disconnected");
-    clients.delete(ws);
-  });
+    ws.on("close", function () {
+        console.log("Player disconnected");
+        clients.delete(ws);
+    });
 });
---
+++ backend/server.ts
@@ -0,0 +1,39 @@
+import WebSocket from "ws";
+
+const clientWs = new WebSocket.Server({ port: 8080 });
+let counter = 0;
+console.log("Server is running...");
+
+const clients = new Map();
+
+clientWs.on("connection", function connection(ws: any) {
+  const id = counter++;
+  console.log("Player connected", id);
+  const color = Math.floor(Math.random() * 360);
+  const metadata = { color, ws };
+
+  clients.set(id, metadata);
+
+  ws.on("message", function incoming(name: any) {
+    console.log("Player received: %s", name);
+    const message = JSON.parse(name);
+    const metadata = clients.get(ws);
+
+    // message.sender = metadata.id;
+    // message.color = metadata.color;
+
+    const outbound = JSON.stringify(message);
+
+    [...clients.keys()].forEach((key) => {
+      if (key === 0) {
+        const a = clients.get(0);
+        a.ws.send(outbound);
+      }
+    });
+  });
+
+  ws.on("close", function () {
+    console.log("Player disconnected");
+    clients.delete(ws);
+  });
+});
