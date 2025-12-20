112313033f78de2fcbf9b87f3a95151771569249
NoemiLengyel <lengyel.noemi96@gmail.com>
2025-12-20 21:14:45 +0100

    Update titles and enhance ready component UI with host indication

--- frontend/projects/player/src/app/phases/00-lobby/components/ready/ready.component.ts
+++ frontend/projects/player/src/app/phases/00-lobby/components/ready/ready.component.ts
@@ -1,19 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, Signal } from '@angular/core';
 import { LobbyService } from '../../lobby.service';
-import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
+import {
+  MatSlideToggleChange,
+  MatSlideToggleModule,
+} from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-ready',
   standalone: true,
   templateUrl: './ready.component.html',
-  styleUrl: './ready.component.scss',
   imports: [MatSlideToggleModule, MatButtonModule],
 })
 export class ReadyComponent {
   lobbyService = inject(LobbyService);
   isReady = this.lobbyService.isReady;
   me = this.lobbyService.me;
+  hostPlayerId = this.lobbyService.hostPlayerId;
+  isHost: Signal<boolean> = computed(
+    () => this.me().id === this.hostPlayerId()
+  );
 
   setReady(event: MatSlideToggleChange) {
     this.lobbyService.setReady(event.checked);
--- frontend/projects/player/src/app/phases/00-lobby/lobby.service.ts
+++ frontend/projects/player/src/app/phases/00-lobby/lobby.service.ts
@@ -2,12 +2,14 @@ import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { joinAttempt, setReadyStatusAttempt } from './state/lobby.actions';
 import { selectIsPlayerReady, selectPlayerObject } from '../../core/state/player/player.selector';
+import { selectHostPlayerId } from '../../../../../game-board/src/app/state/gameboard.selector';
 
 @Injectable({ providedIn: 'root' })
 export class LobbyService {
   store = inject(Store);
   me = this.store.selectSignal(selectPlayerObject);
   isReady = this.store.selectSignal(selectIsPlayerReady);
+  hostPlayerId = this.store.selectSignal(selectHostPlayerId);
 
   join(joinForm: Partial<JoinForm>) {
     this.store.dispatch(joinAttempt({ joinForm }));
